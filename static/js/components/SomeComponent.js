5ff44c731e6f978575391626c30aecac63473a9a
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2020-07-08 21:41:10 +0530

    setup development environment

---
+++ static/js/components/SomeComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo_image from "../../img/react.png";
+// import "./SomeStyle.css";
+
+const SomeComponent = () => {
+    return (
+        <div>
+            <p>
+                <h1 className="some-style-class">Hello, this is a react component.</h1>
+                <br />
+                {/* <img src={logo_image} height="100px" /> */}
+            </p>
+        </div>
+    );
+};
+
+export default SomeComponent;
